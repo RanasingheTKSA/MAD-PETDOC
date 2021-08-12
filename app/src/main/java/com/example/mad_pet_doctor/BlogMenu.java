677ae21b8518db0838c0441d653c7f89ff4df10f
Hiruni Nawanjana <nawanjuhiru2000@gmail.com>
2021-08-13 01:31:00 +0530

    activities created created

---
+++ app/src/main/java/com/example/mad_pet_doctor/BlogMenu.java
@@ -0,0 +1,14 @@
+package com.example.mad_pet_doctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BlogMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blog_menu);
+    }
+}
---
+++ app/src/main/java/com/example/mad_pet_doctor/ChatWithDoc.java
@@ -0,0 +1,14 @@
+package com.example.mad_pet_doctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChatWithDoc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat_with_doc);
+    }
+}
