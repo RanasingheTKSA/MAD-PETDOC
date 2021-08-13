b524df3edf38ca45309582ede9fa6f4e2b79ce92
RanasingheTKSA <sachin.ranasinghe1998.13@gmail.com>
2021-08-13 17:11:44 +0530

    activities are created

---
+++ app/src/main/java/com/example/mad_pet_doctor/DogDiseasesAndVaccines.java
@@ -0,0 +1,14 @@
+package com.example.mad_pet_doctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DogDiseasesAndVaccines extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dog_diseasesandvaccines);
+    }
+}
---
+++ app/src/main/java/com/example/mad_pet_doctor/IntroductionCat.java
@@ -0,0 +1,14 @@
+package com.example.mad_pet_doctor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroductionCat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.introduction_cat);
+    }
+}
